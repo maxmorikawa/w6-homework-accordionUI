7d7fbef821cc4cda586d826509b0c4639b49c5bf
森川雅也 <>
2023-08-16 20:17:46 +0900

    クリックする毎にA1が表示/非表示に切り替わるようにしました

--- accordion.js
+++ accordion.js
@@ -1,13 +1,19 @@
 (() => {
 
-const $elm=document.querySelector("#js-accordion");
-const $trigger= $elm.getElementsByTagName('a');
+const $elm = document.querySelector("#js-accordion");
+const $trigger = $elm.getElementsByTagName('a');
 
 $trigger[0].addEventListener('click', (e) => clickHandler(e));
 
 //クリックしたら実行される処理
 const clickHandler = (e) => {
     e.preventDefault();
-    console.log("Clicked");
+
+    const $content=$trigger[0].nextElementSibling;
+    if($content.style.display === 'block') {
+        $content.style.display = 'none';
+    } else {
+        $content.style.display = 'block';
+    }
 }
 })();
